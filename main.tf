c5fc5867f7e6aa2bae8c5f119680acbeb1330623
Yono1112 <yuma.ohno.sss@gmail.com>
2025-09-05 13:34:41 +0900

    create initial Terraform code

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "6.8.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "keishou-app"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+  auto_create_subnetworks = true
+}
